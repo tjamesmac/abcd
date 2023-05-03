a1bce38e241ebe408e418b25596c6155ef4fe757
Thomas McAuliffe <tjamc@amazon.co.uk>
2023-05-03 16:09:29 +0100

    changed a bunch of stuff

--- main.go
+++ main.go
@@ -37,6 +37,7 @@ type (
 type model struct {
 	textInput textinput.Model
 	directory string
+	path      string
 	err       error
 	keyMaps   map[tea.KeyType]KeyValues
 }
@@ -52,6 +53,67 @@ func initialPath() string {
 	}
 
 	return string(output)
+}
+
+func makeKeyMaps() map[tea.KeyType]KeyValues {
+	keyMapper := make(map[tea.KeyType]KeyValues)
+
+	keyMapper[tea.KeyCtrlA] = KeyValues{value: "a", index: 0}
+	keyMapper[tea.KeyCtrlS] = KeyValues{value: "s", index: 1}
+	keyMapper[tea.KeyCtrlD] = KeyValues{value: "d", index: 2}
+	keyMapper[tea.KeyCtrlQ] = KeyValues{value: "q", index: 3}
+	keyMapper[tea.KeyCtrlW] = KeyValues{value: "w", index: 4}
+	keyMapper[tea.KeyCtrlE] = KeyValues{value: "e", index: 5}
+	keyMapper[tea.KeyCtrlZ] = KeyValues{value: "z", index: 6}
+	keyMapper[tea.KeyCtrlX] = KeyValues{value: "x", index: 7}
+
+	return keyMapper
+}
+
+func listDirectory(path string) string {
+	strippedPath := strings.TrimSuffix(path, "\n")
+	keyMaps := makeKeyMaps()
+	if strippedPath != "" {
+
+		c := exec.Command("ls", strippedPath)
+
+		output, err := c.Output()
+
+		if err != nil {
+			// I got rid of this because it blowing everything up
+			// fmt.Println("Failed to run cmd:", err)
+			// os.Exit(1)
+			return " ls error "
+		}
+
+		list := strings.Split(string(output), "\n")
+		shortcutAppender := func(key tea.KeyType, i int, value string) {
+			stripValue := strings.TrimPrefix(value, "\n")
+			if i == keyMaps[key].index && stripValue != "" {
+				key := keyMaps[key]
+				list[i] = key.value + " " + value
+			}
+		}
+
+		for i, value := range list {
+			shortcutAppender(tea.KeyCtrlA, i, value)
+			shortcutAppender(tea.KeyCtrlS, i, value)
+			shortcutAppender(tea.KeyCtrlD, i, value)
+			shortcutAppender(tea.KeyCtrlQ, i, value)
+			shortcutAppender(tea.KeyCtrlW, i, value)
+			shortcutAppender(tea.KeyCtrlE, i, value)
+			shortcutAppender(tea.KeyCtrlZ, i, value)
+			shortcutAppender(tea.KeyCtrlX, i, value)
+		}
+
+		// 		return strings.Join(list, "\n")
+
+		return strings.Join(list, "\n")
+
+	} else {
+		return " path isnt empty "
+	}
+
 }
 
 func initialModel() model {
@@ -74,7 +136,8 @@ func initialModel() model {
 
 	return model{
 		textInput: ti,
-		directory: lsd("-p"),
+		path:      initialPath(),
+		directory: listDirectory(initialPath()),
 		keyMaps:   keyMapper,
 		err:       nil,
 	}
@@ -92,7 +155,8 @@ func updateInput(m model, inputValue string) {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
-	m.directory = lsd(m.textInput.Value())
+	m.directory = listDirectory(m.textInput.Value())
+	m.path = m.textInput.Value()
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
@@ -108,7 +172,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			tea.KeyCtrlX:
 
 			currentKey := m.keyMaps[msg.Type]
-			list := lsd(m.textInput.Value())
+			list := listDirectory(m.textInput.Value())
 			input := strings.Split(list, "\n")
 
 			var updateValue = m.textInput.Value()
@@ -116,7 +180,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				updateValue = updateValue + "/"
 			}
 
-			m.textInput.SetValue(updateValue + input[currentKey.index])
+			remove_tea_key := strings.Split(input[currentKey.index], " ")
+
+			m.textInput.SetValue(updateValue + remove_tea_key[1])
 			m.textInput.CursorEnd()
 
 			return m, cmd
@@ -146,73 +212,51 @@ func (m model) View() string {
 	return fmt.Sprintf(
 		"%s\n%s",
 		m.textInput.View(),
-		lsd3(m, m.textInput.Value()),
+		listDirectory(m.textInput.Value()),
 	) + "\n"
 }
 
-func lsd(path string) string {
-	// TODO: something like split the path here and if value is not / or . then do nothing
-	// just to make sure it doesn't error when it is going off
-	if path != "" {
-
-		c := exec.Command("ls", path)
-
-		output, err := c.Output()
-
-		if err != nil {
-			// I got rid of this because it blowing everything up
-			// fmt.Println("Failed to run cmd:", err)
-			// os.Exit(1)
-			return ""
-		}
-
-		return string(output)
-	} else {
-		return ""
-	}
-
-}
-
-func lsd3(m model, path string) string {
-	// TODO: something like split the path here and if value is not / or . then do nothing
-	// just to make sure it doesn't error when it is going off
-	// TODO: name this better later
-
-	if path != "" {
-
-		c := exec.Command("ls", path)
-
-		output, err := c.Output()
-
-		if err != nil {
-			// I got rid of this because it blowing everything up
-			// fmt.Println("Failed to run cmd:", err)
-			// os.Exit(1)
-			return ""
-		}
-
-		list := strings.Split(string(output), "\n")
-		shortcutAppender := func(key tea.KeyType, i int, value string) {
-			if i == m.keyMaps[key].index {
-				key := m.keyMaps[key]
-				list[i] = key.value + " " + value
-			}
-		}
-
-		for i, value := range list {
-			shortcutAppender(tea.KeyCtrlA, i, value)
-			shortcutAppender(tea.KeyCtrlS, i, value)
-			shortcutAppender(tea.KeyCtrlD, i, value)
-			shortcutAppender(tea.KeyCtrlQ, i, value)
-			shortcutAppender(tea.KeyCtrlW, i, value)
-			shortcutAppender(tea.KeyCtrlE, i, value)
-			shortcutAppender(tea.KeyCtrlZ, i, value)
-			shortcutAppender(tea.KeyCtrlX, i, value)
-		}
-
-		return strings.Join(list, "\n")
-	} else {
-		return ""
-	}
-
-}
+// func lsd3(m model, path string) string {
+// 	// TODO: something like split the path here and if value is not / or . then do nothing
+// 	// just to make sure it doesn't error when it is going off
+// 	// TODO: name this better later
+// 	stripped := strings.TrimSuffix(path, "\n")
+//
+// 	if stripped != "" {
+//
+// 		c := exec.Command("ls", stripped)
+//
+// 		output, err := c.Output()
+//
+// 		if err != nil {
+// 			// I got rid of this because it blowing everything up
+// 			// fmt.Println("Failed to run cmd:", err.Error())
+// 			// os.Exit(1)
+// 			return " wat " + stripped + " my stripped is zero "
+// 		}
+//
+// 		list := strings.Split(string(output), "\n")
+// 		shortcutAppender := func(key tea.KeyType, i int, value string) {
+// 			if i == m.keyMaps[key].index {
+// 				key := m.keyMaps[key]
+// 				list[i] = key.value + " " + value
+// 			}
+// 		}
+//
+// 		for i, value := range list {
+// 			shortcutAppender(tea.KeyCtrlA, i, value)
+// 			shortcutAppender(tea.KeyCtrlS, i, value)
+// 			shortcutAppender(tea.KeyCtrlD, i, value)
+// 			shortcutAppender(tea.KeyCtrlQ, i, value)
+// 			shortcutAppender(tea.KeyCtrlW, i, value)
+// 			shortcutAppender(tea.KeyCtrlE, i, value)
+// 			shortcutAppender(tea.KeyCtrlZ, i, value)
+// 			shortcutAppender(tea.KeyCtrlX, i, value)
+// 		}
+//
+// 		return strings.Join(list, "\n")
+// 	} else {
+// 		return " wat " + " nothing here "
+// 	}
+//
+// }
